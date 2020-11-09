5b2add922fffd11cba0bba6f6080bfb75730e7e7
steffieharner <steffie.harner@gmail.com>
2020-11-09 12:46:27 +0900

    changed styling for links buttons and updated video to konnyaku park

--- src/pages/links.js
+++ src/pages/links.js
@@ -9,18 +9,18 @@ export default function() {
             <div style={{ minHeight: "600px", textAlign: "center" }}>
                 <div style={{ paddingBottom: "1rem" }}>
                     <a
-                        href="https://youtu.be/V8UjHd3PPYg"
-                        onclick="window.location.href='https://youtu.be/V8UjHd3PPYg'"
-                        className="btn video"
+                        href="https://youtu.be/tzexvpOL_pQ"
+                        onclick="window.location.href='https://youtu.be/tzexvpOL_pQ'"
+                        className="btn links video"
                     >
-                        NEW VID 👉 MY TOKYO APT
+                        NEW VID 👉 KONNYAKU PARK
                     </a>
                 </div>
                 <div>
                     <a
                         href="https://ko-fi.com/steffieharner"
                         onclick="window.location.href='https://ko-fi.com/steffieharner'"
-                        className="btn"
+                        className="btn links"
                     >
                         ☕️ GIVE ME CAFFEINE ^_^
                     </a>
@@ -28,7 +28,7 @@ export default function() {
                 <div>
                     <a
                         href="https://ko-fi.com/steffieharner/commissions"
-                        className="btn"
+                        className="btn links"
                     >
                         💃 MODELING RATES
                     </a>
@@ -36,7 +36,7 @@ export default function() {
                 <div>
                     <a
                         href="https://www.youtube.com/steffieharner"
-                        className="btn"
+                        className="btn links"
                     >
                         📺 YOUTUBE
                     </a>
@@ -44,7 +44,7 @@ export default function() {
                 <div>
                     <a
                         href="https://www.instagram.com/steffieharner"
-                        className="btn"
+                        className="btn links"
                     >
                         📱 INSTAGRAM
                     </a>
@@ -52,7 +52,7 @@ export default function() {
                 <div>
                     <a
                         href="https://www.twitter.com/steffieharner"
-                        className="btn"
+                        className="btn links"
                     >
                         💬 TWITTER
                     </a>
